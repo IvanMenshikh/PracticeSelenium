df93ce38064931caacb8d9f47eb5f2944a661c60
Menshikh Ivan A. <ivashkashox@gmail.com>
2024-04-10 16:15:59 +0300

    second commit

--- src/test/java/OpenWebSite.java
+++ src/test/java/OpenWebSite.java
@@ -1,3 +1,4 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver; //Selenium driver включается после написания команды "WebDriver"
 import org.openqa.selenium.edge.EdgeDriver; //Edge driver включается после написания команды "= new EdgeDriver();"
 
@@ -5,8 +6,7 @@ public class OpenWebSite {
 
     public static void main(String[] args) {
         WebDriver driver = new EdgeDriver(); //driver выступает в данном случае переменной, которую можно обозвать по разному
-        driver.get("https://www.selenium.dev/"); //строчка кода открывает ссылку в браузере
-
-
+        driver.get("https://automationexercise.com/"); //строчка кода открывает ссылку в браузере
+        driver.findElement(By.xpath("//*[@id=\"accordian\"]/div[1]/div[1]/h4/a")).click();
     }
 }

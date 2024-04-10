8b47339ec0761306f71dacfab25b17682cd38f25
Menshikh Ivan A. <ivashkashox@gmail.com>
2024-04-10 16:26:42 +0300

    second

--- src/test/java/OpenWebSite.java
+++ src/test/java/OpenWebSite.java
@@ -6,7 +6,7 @@ public class OpenWebSite {
 
     public static void main(String[] args) {
         WebDriver driver = new EdgeDriver(); //driver выступает в данном случае переменной, которую можно обозвать по разному
-        driver.get("https://automationexercise.com/"); //строчка кода открывает ссылку в браузере
+        driver.get(""); //строчка кода открывает ссылку в браузере
         driver.findElement(By.xpath("//*[@id=\"accordian\"]/div[1]/div[1]/h4/a")).click();
     }
 }
